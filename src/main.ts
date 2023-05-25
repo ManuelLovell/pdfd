755c31839448fdfe38f5169d162fef025595c326
Manuel DeJesus <69539934+ManuelLovell@users.noreply.github.com>
2023-05-24 21:59:05 -0700

    Test6

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { AnnotEventDetail, TsPdfViewerOptions, TsPdfViewer } from "ts-pdf"
 import '/src/style.css'
 
 //const filePath = path.relative("./src/pdf.worker.min.js");
-let url = new URL("lib/foo/bar.js", window.location.origin);
+let url = new URL("./src/pdf.worker.min.js", window.location.origin);
 console.log(url.toString());
 let worker = new Worker(url.toString());
 console.log(worker);
